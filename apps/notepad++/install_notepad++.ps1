4646ee042c66df1446a5dcefa5d919a65c67c1c9
Omar Osman Mahat <74245520+thekingsmakers@users.noreply.github.com>
2024-12-30 07:55:11 +0300

    Update install_notepad++.ps1

--- apps/notepad++/install_notepad++.ps1
+++ apps/notepad++/install_notepad++.ps1
@@ -1,1 +1,4 @@
-
+$notepad++Installer = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.6/npp.8.6.Installer.x64.exe"
+$installerPath = "$env:TEMP\notepad++_installer.exe"
+Invoke-WebRequest -Uri $notepad++Installer -OutFile $installerPath
+Start-Process -FilePath $installerPath -ArgumentList "/S /install" -Wait
