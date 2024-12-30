2c0d629c19b769a5244901cde67607edcc9371b9
Omar Osman Mahat <74245520+thekingsmakers@users.noreply.github.com>
2024-12-30 09:23:29 +0300

    Update install_7-zip.ps1

--- apps/7-zip/install_7-zip.ps1
+++ apps/7-zip/install_7-zip.ps1
@@ -1,4 +1,4 @@
 $7-zipInstaller = "https://github.com/ip7z/7zip/releases/download/24.09/7z2409.msi"
 $installerPath = "$env:TEMP\7-zip_installer.exe"
 Invoke-WebRequest -Uri $7-zipInstaller -OutFile $installerPath
-Start-Process -FilePath $installerPath -ArgumentList "/qn /norestart /install" -Wait
+Start-Process -FilePath $installerPath -ArgumentList "/qn /norestart" -Wait
