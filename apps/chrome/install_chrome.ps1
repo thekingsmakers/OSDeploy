b77bf533f583c74868ced4083b7e32192a2f0393
Omar Osman Mahat <74245520+thekingsmakers@users.noreply.github.com>
2024-12-30 07:48:27 +0300

    Update install_chrome.ps1

--- apps/chrome/install_chrome.ps1
+++ apps/chrome/install_chrome.ps1
@@ -1,1 +1,5 @@
+$chromeInstaller = "https://dl.google.com/chrome/install/latest/chrome_installer.exe"
+$installerPath = "$env:TEMP\chrome_installer.exe"
+Invoke-WebRequest -Uri $chromeInstaller -OutFile $installerPath
+Start-Process -FilePath $installerPath -ArgumentList "/silent /install" -Wait
 
