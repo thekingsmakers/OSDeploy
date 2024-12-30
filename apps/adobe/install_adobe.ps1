c337b1037a66768ed7f32824f4869cefbb18c082
Omar Osman Mahat <74245520+thekingsmakers@users.noreply.github.com>
2024-12-30 10:39:17 +0300

    Update install_adobe.ps1

--- apps/adobe/install_adobe.ps1
+++ apps/adobe/install_adobe.ps1
@@ -1,1 +1,5 @@
+$adobeReaderInstaller = "https://get.adobe.com/reader/download/?installer=Reader_DC_XX_T1_GM&stype=main&standalone=1"
+$installerPath = "$env:TEMP\adobe_reader_installer.exe"
+Invoke-WebRequest -Uri $adobeReaderInstaller -OutFile $installerPath
+Start-Process -FilePath $installerPath -ArgumentList "/sAll /msi" -Wait
 
