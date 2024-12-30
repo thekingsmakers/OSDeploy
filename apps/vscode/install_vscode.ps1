b62e546a8ec859177549eb7d7731604f3757e670
Omar Osman Mahat <74245520+thekingsmakers@users.noreply.github.com>
2024-12-30 07:48:40 +0300

    Update install_vscode.ps1

--- apps/vscode/install_vscode.ps1
+++ apps/vscode/install_vscode.ps1
@@ -1,1 +1,5 @@
+$vscodeInstaller = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64"
+$installerPath = "$env:TEMP\vscode_installer.exe"
+Invoke-WebRequest -Uri $vscodeInstaller -OutFile $installerPath
+Start-Process -FilePath $installerPath -ArgumentList "/silent /install" -Wait
 
